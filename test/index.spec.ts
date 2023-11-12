e3ed64268dbc2e72b2b0ac80eb61c687928b5e7f
Fityan <fityannugroho@gmail.com>
2023-11-12 15:37:05 +0700

    make `wisely` sync, update TS and vitest config (#1)
    * fix: remove `dirname` from vitest config alias
    * refactor: use JSON.parse and change to sync functions
    fix importing JSON with JSON.parse
    * chore: update tsconfig: use "Next" target & module, remove resolve json

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,8 +4,8 @@ import wisely from '~/index.js';
 describe('wisely', () => {
   const text = 'Palestine will be free! Freedom is the right of ALL nations!';
 
-  test('valid', async () => {
-    const result = await wisely({ text });
+  test('valid', () => {
+    const result = wisely({ text });
 
     expect(result).not.equal('');
     expect(result).not.equal(text);
@@ -16,10 +16,10 @@ describe('wisely', () => {
   test.each([
     { phrase: 'palestine', unaffected: 'will be' },
     { phrase: 'free', unaffected: 'Freedom' },
-  ])('with specific phrases provided: $phrase', async ({
+  ])('with specific phrases provided: $phrase', ({
     phrase, unaffected,
   }) => {
-    const result = await wisely({ text, phrases: [phrase] });
+    const result = wisely({ text, phrases: [phrase] });
 
     expect(result).contain(unaffected);
     expect(result).not.match(new RegExp(`\\b${phrase}\\b`, 'i'));
@@ -29,8 +29,8 @@ describe('wisely', () => {
   test.each([
     { testText: 'AABBCCDDz', contains: '48Dz', notContains: '@BC2' },
     { testText: 'aabbccddZ', contains: '@6cd2', notContains: '48Dz' },
-  ])('case sensitive: $testText', async ({ testText, contains, notContains }) => {
-    const result = await wisely({ text: testText, caseSensitive: true });
+  ])('case sensitive: $testText', ({ testText, contains, notContains }) => {
+    const result = wisely({ text: testText, caseSensitive: true });
 
     contains.split('').forEach((char) => {
       expect(result).contain(char);
@@ -43,22 +43,22 @@ describe('wisely', () => {
     expect(result).toHaveLength(testText.length);
   });
 
-  test('no phrases found in the text', async () => {
-    expect(await wisely({ text, phrases: ['foo'] })).toEqual(text);
+  test('no phrases found in the text', () => {
+    expect(wisely({ text, phrases: ['foo'] })).toEqual(text);
   });
 
-  test('empty text', async () => {
-    expect(await wisely({ text: '' })).toEqual('');
+  test('empty text', () => {
+    expect(wisely({ text: '' })).toEqual('');
   });
 
-  test('empty phrases', async () => {
-    expect(await wisely({ text, phrases: [] })).toEqual(text);
+  test('empty phrases', () => {
+    expect(wisely({ text, phrases: [] })).toEqual(text);
   });
 
   test.each([
     { testText: 'AaBbCcDdXxZz', contains: '\u00df\u00d7Zz', notContains: 'AaBbCcDdXx' },
-  ])('with specific charSet (latin-1): $testText', async ({ testText, contains, notContains }) => {
-    const result = await wisely({ text: testText, charSet: 'latin-1' });
+  ])('with specific charSet (latin-1): $testText', ({ testText, contains, notContains }) => {
+    const result = wisely({ text: testText, charSet: 'latin-1' });
 
     contains.split('').forEach((char) => {
       expect(result).contain(char);
